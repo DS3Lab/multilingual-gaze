0a918a40f5ee1646239f1980e35fabf851168194
norahollenstein <nora.hollenstein@gmail.com>
2021-10-26 21:49:58 +0200

    ..

--- processing/model/model.py
+++ processing/model/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from transformers import XLMForTokenClassification, BertForTokenClassification, AdamW, get_linear_schedule_with_warmup
-from transformers import AutoConfig
+from transformers import AutoConfig, AutoModel
 
 
 
