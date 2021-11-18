7bb86c2c1fe4a8473c201c6767f7317d7ceab638
norahollenstein <nora.hollenstein@gmail.com>
2021-11-18 09:18:47 +0100

    ..

--- processing/model/tester.py
+++ processing/model/tester.py
@@ -54,6 +54,7 @@ class Tester(ABC):
             os.makedirs(dir)
 
         for key in self.metrics:
+            print(self.metrics[key])
             self.logs.append(f"{self.metrics[key]:.4f}")
 
         log_text = [self.task] + self.logs + [seed, config.model_pretrained, config.full_finetuning, config.random_weights, config.random_baseline]
