b2f8d340e8b26bc64d4ae30329299bd98363ec6f
norahollenstein <nora.hollenstein@gmail.com>
2022-01-21 09:44:41 +0100

    model saving

--- processing/utils/early_stopping.py
+++ processing/utils/early_stopping.py
@@ -5,7 +5,7 @@ import mlflow
 import torch
 from pathlib import Path
 from processing.settings import LOGGER
-from processing.config import Config
+from processing.utils.config import Config
 
 exec(open('./settings.py').read())
 
