a0a64657a52dde4a9461c8abd9bf6fef1b37fe42
Nora Hollenstein <nora.hollenstein@gmail.com>
2022-10-09 10:19:16 +0200

    Add files via upload

---
+++ cogalex2022/model.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+from transformers import XLMForTokenClassification, BertForTokenClassification, AdamW, get_linear_schedule_with_warmup
+from transformers import AutoConfig, AutoModelForTokenClassification, BertConfig
+
+def randomize_model(model):
+    #https://stackoverflow.com/questions/68058647/initialize-huggingface-bert-with-random-weights
+    for module_ in model.named_modules():
+        if isinstance(module_[1],(torch.nn.Linear, torch.nn.Embedding)):
+            module_[1].weight.data.normal_(mean=0.0, std=model.config.initializer_range)
+        elif isinstance(module_[1], torch.nn.LayerNorm):
+            module_[1].bias.data.zero_()
+            module_[1].weight.data.fill_(1.0)
+        if isinstance(module_[1], torch.nn.Linear) and module_[1].bias is not None:
+            module_[1].bias.data.zero_()
+    return model
+
+class TokenClassificationModel:
+    """
+    Wrapper for the BERT model for token classification. The init
+    method loads the pretrained parameters and stores the init arguments.
+    """
+
+    @classmethod
+    def init(cls, cf, d_out):
+        if "bert" in cf.model_pretrained:
+            model = BertForTokenClassification.from_pretrained(cf.model_pretrained, num_labels=d_out,
+                                    output_attentions=False, output_hidden_states=False)
+            if cf.random_weights is True:
+                # initiate Bert with random weights
+                print("randomizing weights")
+                model = randomize_model(model)
+                print(model.classifier.weight.data)
+            else:
+                # initiate Bert with pre-trained weights
+                print("keeping Bert with pre-trained weights")
+
+                print(model.classifier.weight.data)
+
+        elif "xlm" in cf.model_pretrained:
+            model = XLMForTokenClassification.from_pretrained(cf.model_pretrained, num_labels=d_out, output_attentions=False, output_hidden_states=False)
+
+
+
+        model.d_out = d_out
+
+        return model
+
+
+class GazePredictionLoss:
+    """
+    Loss that deals with a list of variable length sequences. The object call returns global + per-feature MAE loss.
+    """
+
+    def __init__(self, d_gaze):
+        self.d_gaze = d_gaze
+        self.d_report = d_gaze + 1
+
+        self.loss = nn.L1Loss(reduction="sum")
+
+    def __call__(self, b_output, b_target):
+        b_length = [len(i) for i in b_output]
+        losses = torch.zeros(self.d_report)
+
+        losses[0] = sum([self.loss(i, j) for i, j in zip(b_output, b_target)])
+        for output_orig_len, target_orig_len in zip(b_output, b_target):
+            for i in range(1, self.d_report):
+                losses[i] += self.loss(output_orig_len[:, i - 1], target_orig_len[:, i - 1])
+
+        losses[0] /= sum([i * self.d_gaze for i in b_length])
+        losses[1:] /= sum(b_length)
+        return losses
+
+
+def create_finetuning_optimizer(cf, model):
+    """
+    Creates an Adam optimizer with weight decay. We can choose whether to perform full finetuning on
+    all parameters of the model or to just optimize the parameters of the final classification layer.
+    """
+    if cf.full_finetuning:
+        param_optimizer = list(model.named_parameters())
+        no_decay = ["bias"]
+        optimizer_grouped_parameters = [
+            {"params": [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+             "weight_decay_rate": cf.weight_decay},
+            {"params": [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+             "weight_decay_rate": 0}
+        ]
+    else:
+        param_optimizer = list(model.classifier.named_parameters())
+        optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
+
+    return AdamW(optimizer_grouped_parameters, lr=cf.lr, eps=cf.eps)
+
+
+def create_scheduler(cf, optim, dl):
+    """
+    Creates a linear learning rate scheduler.
+    """
+    n_iters = cf.n_epochs * len(dl)
+    return get_linear_schedule_with_warmup(optim, num_warmup_steps=0, num_training_steps=n_iters)
---
+++ cogalex2022/test.py
@@ -0,0 +1,74 @@
+from settings import DEVICE, reproducibility_setup, RANDOM_STATE
+from transformers import AutoConfig
+
+reproducibility_setup()
+
+import argparse
+import os
+import torch
+
+from processing import TokenClassificationModel, GazeTester, GazeDataLoader, Config, GazeDataset, \
+    create_tokenizer, load_json, LOGGER
+
+
+def main(args):
+    data_gaze_dir = args.data_gaze_dir
+    results_gaze_dir = args.results_gaze_dir
+    tasks = args.tasks
+
+    cf = Config.load_json(os.path.join(results_gaze_dir, "config.json"))
+
+    tokenizer = create_tokenizer(cf.model_pretrained)
+
+    for task in tasks:
+
+        if args.test_task is None:
+            test_task = task
+        else:
+            test_task = args.test_task
+
+        results_task_dir = os.path.join(results_gaze_dir, task)
+
+        model_init_args = load_json(os.path.join(results_task_dir, "model_init.json"))
+
+        LOGGER.info("initiating random Bert model: ")
+        LOGGER.info(cf.random_weights)
+        model = TokenClassificationModel.init(cf, **model_init_args)
+
+        if not cf.random_baseline:
+            # set random_baseline to True in the cf file loaded above to test on a randomly initialized regression
+            LOGGER.info("Fine-tuned on eye-tracking data!")
+            model.load_state_dict(torch.load(os.path.join(results_task_dir, "model-"+cf.model_pretrained+"-"+cf.full_finetuning+"-"+str(RANDOM_STATE)+".pth")))
+            print(model.classifier.weight.data)
+        else:
+            LOGGER.info("Random regression layer, NO trained weights loaded!")
+            print(model.classifier.weight.data)
+
+        d = GazeDataset(cf, tokenizer, os.path.join(data_gaze_dir, test_task), test_task)
+        d.read_pipeline()
+
+        dl = GazeDataLoader(cf, d.numpy["test"], d.target_pad, mode="test")
+
+        #LOGGER.info(model)
+
+        tester = GazeTester(model, dl, DEVICE, task)
+        tester.evaluate()
+
+        eval_dir = os.path.join(results_gaze_dir, task)
+        tester.save_preds(os.path.join(eval_dir, "preds-"+str(RANDOM_STATE)+"-"+cf.model_pretrained.replace("/","")+".csv"))
+        tester.save_logs(os.path.join(eval_dir, "results."+str(RANDOM_STATE)+"-"+cf.model_pretrained.replace("/","")+".log"))
+        tester.save_logs_all(os.path.join(results_gaze_dir, "result_log.csv"), RANDOM_STATE, cf)
+        LOGGER.info(f"Testing completed, training on task {task}, testing on {test_task}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-dg", "--data_gaze_dir", type=str,
+                        default="../../data/gaze")
+    parser.add_argument("-rg", "--results_gaze_dir", type=str,
+                        default="../../results/gaze")
+    parser.add_argument("-ts", "--tasks", type=str, nargs="+",
+                        default=["zuco"])
+    parser.add_argument("-tt", "--test_task", type=str, default=None)
+    args = parser.parse_args()
+    main(args)
